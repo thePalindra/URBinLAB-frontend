14c43de337d3415e2643c663aac73a596d35160d
Palindra <rjp.goncalves@campus.fct.unl.pt>
2022-08-06 17:30:39 +0100

    God helped me so now it's kind of working

---
+++ frontend/src/pages/AttachSpace.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AttachSpace from "../components/AttachSpace"
+import Header from "../components/Header"
+
+export default function defaultFunction() {
+    return (
+        <>
+            <AttachSpace/>
+        </>
+    )
+}
